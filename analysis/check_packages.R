98c4f98c429f14381db08b448a686d9e5d725271
Aaron Gullickson <aarongullickson@gmail.com>
2021-02-22 20:38:58 -0800

    remove custom color packages

--- analysis/check_packages.R
+++ analysis/check_packages.R
@@ -7,8 +7,7 @@
 
 #add new packages to the chain here
 packages = c("here","readr","ggplot2","texreg","devtools","dplyr","tibble",
-             "reshape2", "survival", "LaCroixColoR", "wesanderson",
-             "knitr","ggstance")
+             "reshape2", "survival","knitr","ggstance")
 
 package.check <- lapply(packages, FUN = function(x) {
   if (!require(x, character.only = TRUE)) {
